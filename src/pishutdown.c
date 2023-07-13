7987c3d28f900f2a899f260be7ae9cdf2ff14ef9
Simon Long <simon@raspberrypi.com>
2023-07-13 13:36:20 +0100

    Update env test

--- src/pishutdown.c
+++ src/pishutdown.c
@@ -82,7 +82,7 @@ int main (int argc, char *argv[])
     textdomain (GETTEXT_PACKAGE);
 #endif
 
-    if (getenv ("WAYFIRE_CONFIG_FILE")) wayfire = TRUE;
+    if (getenv ("WAYLAND_DISPLAY")) wayfire = TRUE;
 
     // GTK setup
     gtk_init (&argc, &argv);
