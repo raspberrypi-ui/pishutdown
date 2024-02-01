265bc7729c810a77ea50584ee0f801d4dc09e7a8
Simon Long <simon@raspberrypi.com>
2024-02-01 13:41:02 +0000

    Enable use of systemd-logind

--- src/pishutdown.c
+++ src/pishutdown.c
@@ -42,7 +42,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <libudev.h>
 #include <linux/input.h>
 
-//#define USE_LOGIND
+#define USE_LOGIND
 
 #ifdef USE_LOGIND
 GDBusProxy *proxy;
